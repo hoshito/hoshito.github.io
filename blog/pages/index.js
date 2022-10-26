168475686bdaedf1f0f355f919784d1c5019b118
hoshito <10190139+hoshito@users.noreply.github.com>
2022-10-26 14:03:31 +0000

    add date in index

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -12,6 +12,7 @@ export default function Home({ blog }) {
                 <img className="w-full h-auto rounded-t-xl" src={blog.eyecatch.url} />
                 <div className="p-4 md:p-5">
                   <h3 className="text-lg font-bold text-gray-800 dark:text-white">{blog.title}</h3>
+                  <p className="text-sm">{formatDate(blog.publishedAt)}</p>
                 </div>
               </div>
             </a>
@@ -32,3 +33,23 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+function zeroPadding(val, digit = 2) {
+  return ("0" * digit + val.toString()).slice(-digit);
+}
+
+function formatDate(str) {
+  if (!str) { return }
+  const date = new Date(str);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const d = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  return year.toString() + "/" +
+    zeroPadding(month) + "/" +
+    zeroPadding(d) + " " +
+    zeroPadding(hours) + ":" +
+    zeroPadding(minutes);
+}
